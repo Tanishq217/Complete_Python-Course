9befe21c051a9db9ed022c1f80bbf289880075f5
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2025-07-05 00:27:25 +0530

    Learned how to take input in python

---
+++ 07_Input.py
@@ -0,0 +1,19 @@
+x = input("x : ")
+# Input is always a string
+# "x" is a string
+print(type(x))
+print("we are converting x to integer")
+x = int(x)  # Convert input to integer
+
+
+print(type(x))
+y = x + 10
+print("y : ", y)
+
+
+# Falsy Values
+# Falsy values in Python
+# 0, None, False, "", [], {}, set(), ()
+# These values are considered false in boolean context
+
+
